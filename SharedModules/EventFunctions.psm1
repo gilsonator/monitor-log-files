13e6858f74d8708c64a0a707f56630b83dfe3568
Dave Gilson <gilsond@gmail.com>
2024-08-28 19:18:38 +1000

    More changes, added .gitignore

--- SharedModules/EventFunctions.psm1
+++ SharedModules/EventFunctions.psm1
@@ -1,32 +1,34 @@
-# EventFunctions.ps1
+# Helper functions
+function Convert-TimeToMilliseconds {
+    param (
+        [int]$Hours
+    )
+    return $Hours *  3600000
+}
 
+function Format-EventLevelsSQL {
+  param (
+    [string] $Levels
+  )
+
+  return $EventLevels -replace ",", " or Level="
+}
+
+# Public functions
 function Get-FilterXml {
     param (
-        [string]$LogPath = "Microsoft-Windows-WindowsUpdateClient/Operational",
+        $LogPath,
         [string]$EventLevels,
-        [int]$TimeDiff = 3600000 # Default to last hour in milliseconds
+        $TimeDiff = 3600000 # Default to last hour in milliseconds
     )
-
-    $eventLevelsFormatted = $EventLevels -replace ",", " or Level="
-
-    return @"
+    
+    
+    Format-EventLevelsSQL()
+ @"
 <QueryList>
   <Query Id="0" Path="$LogPath">
-    <Select Path="$LogPath">*[System[(Level=$eventLevelsFormatted) 
-        and TimeCreated[timediff(@SystemTime) <= $TimeDiff]]]</Select>
+    <Select Path="$LogPath">*[System[(Level=$eventLevelsFormatted) and TimeCreated[timediff(@SystemTime) &lt;=$TimeDiff]]]</Select>
   </Query>
 </QueryList>
 "@
-}
-
-function Get-Events {
-    param (
-        [string]$EventLevels,
-        [int]$TimeDiff
-    )
-    $filterXml = Get-FilterXml -EventLevels $EventLevels
-    Write-Output $filterXml
-    
-    $events = Get-WinEvent -FilterXml $filterXml | Sort-Object TimeCreated -Descending 
-    return $events
 }
--- WindowsUpdateLogExtract/WindowsUpdateLogXMFilter.ps1
+++ WindowsUpdateLogExtract/WindowsUpdateLogXMFilter.ps1
@@ -1,18 +1,18 @@
 <#
 .SYNOPSIS
-    Simple Windows Update Log Log Filter - Using Get-WindowsUpdateLog creates a Tempory XML, uses a regular expression to extract specific details.
+    Simple Windows Event Filter - Using Get-WindowsUpdateLog creates a Tempory XML, uses a regular expression to extract specific details.
 
 .DESCRIPTION
-    This script monitors the CBS log file, highlighting different log levels with colours.
+    This script monitors the Windows Update log file, highlighting different log levels with colours.
     It is similar to the Linux 'tail -F' command.
 
     By David Gilson
+    
+    Found LogName using:
+    Get-WinEvent -ListLog '*Update*' | Select-Object -Property 'LogName'
 
 .LINK
-    https://learn.microsoft.com/en-us/powershell/module/windowsupdate/get-windowsupdatelog?view=windowsserver2022-ps
-
-.PARAMETER FileName
-    The path to the CBS log file. Default is "$env:SystemRoot\Logs\CBS\CBS.log".
+    https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.diagnostics/get-winevent?view=powershell-7.4 
 
 .PARAMETER TailLines
     The number of lines to display from the end of the log file. Default is 0 so only new lines output.
@@ -24,32 +24,44 @@
     Display only specific log levels. "All", "Info", "Warning", "Error". Default is "All".
 
 .EXAMPLE
-    .\Monitor-CBSLog.ps1 -FileName "C:\Windows\Logs\CBS\CBS.log" -TailLines 20 -Delay 2 -Level "Error"
-
-    This example runs the script with a specified log file, displays the last 20 lines of "Error", and sets a delay of 2 seconds between checks.
+    
 #>
 param (
     [ValidatePattern("^\d+(,\d+)*$")]
-    [string]$EventLevels = "1,2,3,4",  # Default to levels 1, 2, 3 and 4
+    [string]$EventLevels = "0,1,2,3,4,5",  # Default to levels 1, 2, 3 and 4
 
     [ValidateRange(1, 100)]
     [int]$EventCount = 10,  # Default to showing the last 10 events
 
-    [int]$HoursDiffFromNow = 1 # Default to last hour from current time
+    [int]$Hours = 12 # Number of Hours to show, defaults to 12
 )
 
-# Import the functions from EventFunctions.ps1
-Import-Module .\SharedModules\EventFunctions.psm1
+$MilliSeconds = (60 * 60 * 1000) * $Hours
 
-# Continuously monitor and display new events
-#while ($true) {
-    #Clear-Host  # Clear the console for better readability
-    $events = Get-Events -EventCount $EventCount -EventLevels $eventLevelsFormatted -TimeDiff $startTimeDiff 
-    foreach ($event in $events) {
-        $eventTime = $event.TimeCreated
-        $eventId = $event.Id
-        $eventMessage = $event.Message
-        Write-Output "Time: $eventTime, Event ID: $eventId, Message: $eventMessage"
-    }
-#   Start-Sleep -Seconds 5  # Adjust the sleep interval as needed
-#}
+# Using the FilterXML parameter:
+$xmlQuery = @"
+<QueryList>
+  <Query Id="0" Path="Microsoft-Windows-WindowsUpdateClient/Operational">
+    <Select Path="Microsoft-Windows-WindowsUpdateClient/Operational">*[System[(Level=1  or Level=2 or Level=3 or Level=4 or Level=0) and TimeCreated[timediff(@SystemTime) &lt;=$MilliSeconds]]]</Select>
+  </Query>
+</QueryList>
+"@
+
+Write-Debug $xmlQuery
+Get-WinEvent -FilterXML $xmlQuery
+
+# DG: Other ways...
+# Using the Where-Object cmdlet:
+
+#$PastDate = (Get-Date) - (New-TimeSpan -Hours $HoursDiff)
+#Get-WinEvent -LogName 'Microsoft-Windows-WindowsUpdateClient/Operational' 
+#    | Where-Object { $_.TimeCreated -ge $PastDate }
+    
+
+# Using the FilterHashtable parameter:
+# $Yesterday = (Get-Date) - (New-TimeSpan -Day 1)
+# Get-WinEvent -FilterHashtable @{ LogName='Windows PowerShell'; Level=3; StartTime=$Yesterday }
+
+# Using the FilterXPath parameter:
+# $XPath = '*[System[Level=3 and TimeCreated[timediff(@SystemTime) &lt;= 86400000]]]'
+# Get-WinEvent -LogName 'WindowsUpdateClient' -FilterXPath $XPath
