3c05be277c9ca5f512b05a3db9332802c6367a59
Dave Gilson <gilsond@gmail.com>
2024-08-31 22:17:45 +1000

    More updates

--- SharedModules/EventFunctions.psm1
+++ SharedModules/EventFunctions.psm1
@@ -1,32 +1,76 @@
 # Helper functions
-function Convert-TimeToMilliseconds {
-    param (
-        [int]$Hours
-    )
-    return $Hours *  3600000
-}
 
-function Format-EventLevelsSQL {
+function Get-TimeInMilliseconds {
+  [CmdletBinding(DefaultParameterSetName = 'HoursSet')]
   param (
-    [string] $Levels
+      [Parameter(ParameterSetName = 'HoursSet', Mandatory = $false)]
+      [ValidateRange(1, 24)]
+      [int]$Hours = 24,
+
+      [Parameter(ParameterSetName = 'DaysSet', Mandatory = $false)]
+      [ValidateRange(1, 7)]
+      [int]$Days = 1,
+
+      [Parameter(ParameterSetName = 'WeeksSet', Mandatory = $false)]
+      [ValidateRange(1, 4)]
+      [int]$Weeks = 1
   )
 
-  return $EventLevels -replace ",", " or Level="
+  switch ($PSCmdlet.ParameterSetName) {
+      'HoursSet' { $MilliSeconds = 3600000 * $Hours } # (60 * 60 * 1000)
+      'DaysSet' { $MilliSeconds = 86400000 * $Days }  # (24 * 60 * 60 * 1000)
+      'WeeksSet' { $MilliSeconds = 604800000 * $Weeks } # (7 * 24 * 60 * 60 * 1000)
+  }
+
+  Write-Output "Milliseconds: $MilliSeconds"
 }
 
-# Public functions
-function Get-FilterXml {
-    param (
-        $LogPath,
-        [string]$EventLevels,
-        $TimeDiff = 3600000 # Default to last hour in milliseconds
-    )
+# Usage examples:
+# Get-TimeInMilliseconds -Hours 12  # Uses HoursSet
+# Get-TimeInMilliseconds -Days 2    # Uses DaysSet
+# Get-TimeInMilliseconds -Weeks 1   # Uses WeeksSet
 
- @"
+function Get-EventQuery {
+  [CmdletBinding(DefaultParameterSetName = 'HoursSet')]
+  param (
+      [Parameter(ParameterSetName = 'HoursSet', Mandatory = $false)]
+      [ValidateRange(1, 24)]
+      [int]$Hours = 24,
+
+      [Parameter(ParameterSetName = 'DaysSet', Mandatory = $false)]
+      [ValidateRange(1, 7)]
+      [int]$Days = 1,
+
+      [Parameter(ParameterSetName = 'WeeksSet', Mandatory = $false)]
+      [ValidateRange(1, 4)]
+      [int]$Weeks = 1,
+
+      [Parameter(Mandatory = $false)]
+      [ValidateSet("Verbose", "Critical", "Error", "Warning", "Information")]
+      [string[]]$EventLevels = @("Information")
+  )
+
+  switch ($PSCmdlet.ParameterSetName) {
+      'HoursSet' { $MilliSeconds = 3600000 * $Hours } # (60 * 60 * 1000)
+      'DaysSet' { $MilliSeconds = 86400000 * $Days }  # (24 * 60 * 60 * 1000)
+      'WeeksSet' { $MilliSeconds = 604800000 * $Weeks } # (7 * 24 * 60 * 60 * 1000)
+  }
+
+  $LevelsQuery = $EventLevels -join " or "
+
+  $xmlQuery = @"
 <QueryList>
-  <Query Id="0" Path="$LogPath">
-    <Select Path="$LogPath">*[System[(Level=$eventLevelsFormatted) and TimeCreated[timediff(@SystemTime) &lt;=$TimeDiff]]]</Select>
-  </Query>
+<Query Id="0" Path="Microsoft-Windows-WindowsUpdateClient/Operational">
+  <Select Path="Microsoft-Windows-WindowsUpdateClient/Operational">*[System[($LevelsQuery) 
+      and TimeCreated[timediff(@SystemTime) <=$MilliSeconds]]]</Select>
+</Query>
 </QueryList>
 "@
+
+  Write-Output $xmlQuery
 }
+
+# Usage examples:
+# Get-EventQuery -Hours 12  # Uses HoursSet
+# Get-EventQuery -Days 2    # Uses DaysSet
+# Get-EventQuery -Weeks 1   # Uses WeeksSet
--- WindowsUpdateLogExtract/WindowsUpdateLogXMFilter.ps1
+++ WindowsUpdateLogExtract/WindowsUpdateLogXMFilter.ps1
@@ -15,14 +15,14 @@
     https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.diagnostics/get-winevent?view=powershell-7.4 
 
 .PARAMETER EventLevels
-    Display only specific event levels, separated by commas. Defaults to (1,2,3).
-    0 (Verbose)     1 (Critical)     2 (Error)     3 (Warning)     4 (Information) 
+    Display only specific event levels, separated by commas. Defaults to ("Information").
+    "Verbose", "Critical", "Error", "Warning", "Information"
 
 .PARAMETER Hours
     Display only specific events in the last number of hours from now. Defaults to the last 24 hours.
 
 .EXAMPLE
-    WindowsUpdateLogXMLFilter.ps1 -EventLevels (1,2,3) -Hours 24
+    WindowsUpdateLogXMLFilter.ps1 -EventLevels Critical,Error,Warning -Hours 24
     Will search for Critical, Error, or Warning events recorded in the last 24 hours from now.
 #>
 param (
@@ -62,7 +62,7 @@ Write-Debug $LevelsQuery
 
 $MilliSeconds = 3600000 * $Hours # (60 * 60 * 1000)
 
-# I know XML very much - So I decided to use the FilterXML parameter:
+# I know XML very well - So I decided to use the FilterXML parameter:
 $xmlQuery = @"
 <QueryList>
   <Query Id="0" Path="Microsoft-Windows-WindowsUpdateClient/Operational">
@@ -76,24 +76,44 @@ Write-Debug $xmlQuery
 
 try {
     $events = Get-WinEvent -FilterXML $xmlQuery -ErrorAction Stop
-    $events #| Format-Table -AutoSize 
+    # $events | Format-Table -AutoSize 
+
+    Write-Host "TimeCreated`t`tId`tMessage" -ForegroundColor Blue
+    foreach ($event in $events) {
+        Write-Host $event.TimeCreated -ForegroundColor Green -NoNewline
+        Write-Host "`t" -NoNewline
+        Write-Host $event.Id -ForegroundColor Yellow -NoNewline
+        Write-Host "`t" -NoNewline
+        Write-Host $event.Message -ForegroundColor Red
+    }
 }
 catch {
-    $formattedString = "No Windows Update Events recorded in the past "
+    
     $formattedNumber = "{0:N0}" -f $Hours
+    # Modified from this...
+    <# 
+    $formattedString = "No Windows Update Events ($($EventLevels -join ', ')) recorded in the past "
     if ($Hours -gt 1) { 
-        $formattedString += $formattedNumber + " hours." 
+       $formattedString += $formattedNumber + " hours." 
     } else { 
-        $formattedString += $formattedNumber + " hour." 
+       $formattedString += $formattedNumber + " hour." 
     }
-    Write-Host $formattedString
+    Write-Host $formattedString -ForegroundColor Yellow 
+    Write-Host ""
+    #>
+    # To this: to combine in one line, Which is better? I feel the above is easier to read, and debug
+    
+    Write-Host -ForegroundColor Yellow @"
+No Windows Update Events ($($EventLevels -join ', ')) returned in the past $formattedNumber $($Hours -gt 1 ? "hours." : "hour." )
+
+"@
 }
 
-# DG: Other ways...
+# DG: Other ways, not complete...
 # Using the Where-Object cmdlet:
 
-#$PastDate = (Get-Date) - (New-TimeSpan -Hours $HoursDiff)
-#Get-WinEvent -LogName 'Microsoft-Windows-WindowsUpdateClient/Operational' 
+# $PastDate = (Get-Date) - (New-TimeSpan -Hours $HoursDiff)
+# Get-WinEvent -LogName 'Microsoft-Windows-WindowsUpdateClient/Operational' 
 #    | Where-Object { $_.TimeCreated -ge $PastDate }
     
 
