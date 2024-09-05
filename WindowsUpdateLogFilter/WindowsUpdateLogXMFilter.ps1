99d2b535b7b8963ff0629cefaa6abd33fc159172
Dave Gilson <gilsond@gmail.com>
2024-09-05 14:40:20 +1000

    Added option to export to .csv file or output only

--- WindowsUpdateLogFilter/WindowsUpdateLogXMFilter.ps1
+++ WindowsUpdateLogFilter/WindowsUpdateLogXMFilter.ps1
@@ -21,6 +21,9 @@
 .PARAMETER Hours
     Display only specific events in the last number of hours from now. Defaults to the last 24 hours.
 
+.PARAMETER ExportCSVPath
+    Create a .csv file based on date/time, instead of outputting. Defaults to users temp folder
+
 .EXAMPLE
     WindowsUpdateLogXMLFilter.ps1 -EventLevels Critical,Error,Warning -Hours 24
     Will search for Critical, Error, or Warning events recorded in the last 24 hours from now.
@@ -31,9 +34,28 @@ param (
     [string[]]$EventLevels = @("Information"),
 
     [Parameter(Mandatory=$false)]
-    [ValidateRange(1, 24)]
-    [int]$Hours = 24 # -Hours 24 
+    [ValidateRange(1, 72)]
+    [int]$Hours = 24, # -Hours 24 
+
+    [Parameter(Mandatory=$false)]
+    [bool]$ExportCSV = $false,
+
+    [Parameter(Mandatory=$false)]
+    $ExportCSVPath = $env:Temp
 )
+
+if ($ExportCSV) {
+    # Resolve the ExportCSVPath path relative to the script's directory
+    if ([System.IO.Path]::IsPathRooted($ExportCSVPath)) {
+        $exportCSVPath = $ExportCSVPath
+    } else {
+        $exportCSVPath = Join-Path -Path $PSScriptRoot -ChildPath $ExportCSVPath
+    }
+
+    $DateTime = Get-Date -Format "yyyyMMdd_HHmmss"
+    $exportCSVPath += "\WindowsUpdateLogExport_$DateTime.csv"
+    
+}
 
 # Clear-Host
 # $ErrorActionPreference = "Stop"
@@ -72,33 +94,57 @@ $xmlQuery = @"
 </QueryList>
 "@
 
-Write-Debug $xmlQuery
+$events = Get-WinEvent -FilterXML $xmlQuery -ErrorAction Stop
+# Another way in XML, is using the FilterXPath parameter:
+# NOTE: The timediff function works within the XML query format used by Get-WinEvent -FilterXML. 
+#       The -FilterXPath parameter has limitations and doesn’t support all XPath functions, 
+#       which is why the timediff function doesn’t work:
+# $XPath = "*[System[($LevelsQuery) and TimeCreated[timediff(@SystemTime) &lt;=$MilliSeconds]]]"
+# $events = Get-WinEvent -LogName 'Microsoft-Windows-WindowsUpdateClient/Operational' -FilterXPath $XPath
 
-try {
-    $events = Get-WinEvent -FilterXML $xmlQuery -ErrorAction Stop
+if ($ExportCSV) {
+    $filteredEvents = @()
+} else {
+    Write-Host "Date`t`t`tLevel`t`tMessage`t`tDetails" -ForegroundColor Blue 
+}
 
-    Write-Host "Date`t`t`tLevel`t`tMessage" -ForegroundColor Blue
+if ($events.Count -eq 0) {
+    Write-Host  "No Windows Update Events ($($EventLevels -join ', '))" `
+                "returned in the past $($Hours -gt 1 ? "{0:N0} hours" -f $Hours : "hour")" -ForegroundColor Yellow
+} else {
     foreach ($event in $events) {
-        Write-Host $event.TimeCreated -ForegroundColor Green -NoNewline
-        Write-Host "`t" -NoNewline
-        Write-Host $event.LevelDisplayName -ForegroundColor Yellow -NoNewline
-        Write-Host "`t" -NoNewline
-        Write-Host $event.Message -ForegroundColor White
+        if ($ExportCSV) {
+            $filteredEvent  = [PSCustomObject]@{
+                TimeCreated = $event.TimeCreated
+                Level       = $event.LevelDisplayName
+                Message     = $event.Message
+                Data        = if ($event.Opcode -eq 12) { $event.Properties[0].Value }
+            }
+            # Add the filtered event to the array
+            $filteredEvents += $filteredEvent
+        } else { # Write to host
+            Write-Host $event.TimeCreated -ForegroundColor Green -NoNewline
+            Write-Host "`t" -NoNewline
+            Write-Host $event.LevelDisplayName -ForegroundColor Yellow -NoNewline
+            Write-Host "`t" -NoNewline
+            Write-Host $event.Message -ForegroundColor White -NoNewline
+            # For downloads, show package
+            Write-Host "`t" -NoNewline
+            Write-Host ($event.Opcode -eq 12 ? $event.Properties[0].Value : "") -ForegroundColor Magenta
+        }
+    }
+
+    # Export csv..
+    if ($ExportCSV) {
+        $filteredEvents | Export-Csv -Path $exportCSVPath
+        Write-Host $events.Count "Windows Update Events ($($EventLevels -join ', ')) exported to" $exportCSVPath `
+            -ForegroundColor Yellow
     }
 }
-catch {
-    Write-Host  "No Windows Update Events ($($EventLevels -join ', '))" `
-                " returned in the past $($Hours -gt 1 ? "{0:N0} hours" -f $Hours : "hour")" -ForegroundColor Yellow
-}
 
-# DG NOTES: There are other ways to accomplish this:
+# DG NOTES: There are other ways to accomplish this, refer to above link:
 #
-# Using the FilterXPath parameter (simpler):
-# $XPath = "*[System[($LevelsQuery) and TimeCreated[timediff(@SystemTime) &lt;= $MilliSeconds]]]"
-# Get-WinEvent -LogName 'Microsoft-Windows-WindowsUpdateClient/Operational' -FilterXPath $XPath
-
 # Not Completed:
-
 # Using the Where-Object cmdlet:
 # $PastDate = (Get-Date) - (New-TimeSpan -Hours $HoursDiff)
 # Get-WinEvent -LogName 'Microsoft-Windows-WindowsUpdateClient/Operational' 
